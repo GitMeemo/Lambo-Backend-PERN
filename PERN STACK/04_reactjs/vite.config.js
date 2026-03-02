e554983a7bbeddb2f5700ec76054469c90bcab1e
GitMeemo <ameenkamangar@gmail.com>
2026-03-03 02:40:55 +0500

    Lambo Backend Finishes

--- PERN STACK/04_reactjs/vite.config.js
+++ PERN STACK/04_reactjs/vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // This Vite proxy forwards all /api requests from
+  // the frontend to http://localhost:3000,
+  // avoiding CORS issues during development.
   server: {
     proxy: {
-      '/api': { // Updated from './api' to '/api'
+      '/api': { 
         target: 'http://localhost:3000',
         changeOrigin: true,
       }
